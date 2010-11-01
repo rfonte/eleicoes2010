d48c66793c302cc3d592eccf08cc5e0c3525ebfa
Rodrigo <rodrigo.fonte@yahoo.com.br>
2010-11-01 18:17:25 -0200

    alteração menu + banner + video

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,8 +1,13 @@
 $(document).ready (function() {
+
     $("#menu>li").hover (function() {
         $("#menu>li ul").hide();
         $(this).children("ul").show();
-    })
+    }),
 
+    $("#menu>li").mouseleave (function() {
+        $("#menu>li ul").hide();
+        $(this).children("ul").hide();
+    })
 });
 
